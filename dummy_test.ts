9028b9ddea3a4580ab41b54d206d66c40e023500
Kyoh <me@kyoh86.dev>
2024-04-05 04:25:17 +0900

    Initial commit

---
+++ dummy_test.ts
@@ -0,0 +1,10 @@
+import { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
+import { assert } from "https://deno.land/std@0.221.0/assert/mod.ts";
+
+test({
+  mode: "all",
+  name: "dummy",
+  fn: () => {
+    assert(true);
+  },
+});
