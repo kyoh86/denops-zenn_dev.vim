6e64f4943112d3de51b5109fa30b727922eb1c5a
GitHub <noreply@github.com>
2024-04-06 09:04:47 +0000

    chore(deps): Update deno dependencies

--- denops/boilerplate/main.ts
+++ denops/boilerplate/main.ts
@@ -3,7 +3,7 @@ import {} from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
 import { join } from "https://deno.land/std@0.221.0/path/mod.ts";
 import { ensureFile } from "https://deno.land/std@0.221.0/fs/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
 import {
   ConsoleHandler,
   getLogger,
